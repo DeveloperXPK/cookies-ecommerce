c1843c03929aae27bff07f2c37b119a3a4d9b498
DeveloperXPK <andresgiraldo05a@gmail.com>
2025-02-27 17:01:59 -0500

    Eliminar rutas y controladores de cajas, galletas y órdenes del backend

--- Backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/cajas", require("./cajaRoutes"));
-router.use("/galletas", require("./galletaRoutes"));
-router.use("/ordenes", require("./ordenRoutes"));
-
-module.exports = router;
